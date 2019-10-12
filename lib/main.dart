008f2be443765aecdb477b753420af98dfad6aa8
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-12 15:57:45 -0500

    primera app en iOS

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Te quiero mucho mi AMOR!',
             ),
             Text(
               '$_counter',
