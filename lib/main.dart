8e7174bb3a0571c790c0618972326b3e259c1f8e
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-13 22:13:15 -0500

    Se elimino todo el código y se hizo el main con runApp y una clase llamada MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/material.dart';
 
 void main() {
+	runApp(new MyApp());
+}
 
+class MyApp extends StatelessWidget {
+	@override
+	Widget build(context) {
+		return MaterialApp();
+	}
 }
